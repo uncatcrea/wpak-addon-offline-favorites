2476174fc371b37d229d737b94555025c8a40d86
Lionel Pointet <lionel.pointet@virtua.ch>
2015-06-15 18:09:20 +0200

    Add second JS file

--- wp-appkit-favorites.php
+++ wp-appkit-favorites.php
@@ -24,6 +24,7 @@ if ( !class_exists( 'WpAppKitFavorites' ) ) {
             $addon->set_location( __FILE__ );
 
             $addon->add_js( 'js/wpak-favorites.js', 'module' );
+            $addon->add_js( 'js/wpak-favorites-app.js', 'theme', 'before' );
 
             $addons[] = $addon;
 
