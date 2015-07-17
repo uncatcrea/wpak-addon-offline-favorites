864baca1d6f9b81a5943d28d255f9a9e122f3af5
Lionel Pointet <lionel.pointet@gmail.com>
2015-07-17 17:13:30 +0200

    Fix #2: The 'Favorite' component is available in component list even if addon not activated for the app

--- wpak-addon-favorites.php
+++ wpak-addon-favorites.php
@@ -13,9 +13,6 @@ if ( !class_exists( 'WpAppKitFavorites' ) ) {
 
         public static function hooks() {
             add_filter( 'wpak_addons', array( __CLASS__, 'wpak_addons' ) );
-
-            // 11 to pass after WP-AppKit components declaration
-            add_action( 'plugins_loaded', array( __CLASS__, 'plugins_loaded' ), 11 );
         }
 
         public static function wpak_addons( $addons ) {
@@ -28,13 +25,11 @@ if ( !class_exists( 'WpAppKitFavorites' ) ) {
             $addon->add_js( 'js/view.js', 'module' );
             $addon->add_js( 'js/wpak-favorites-app.js', 'theme', 'before' );
 
+            $addon->require_php( dirname(__FILE__) .'/component-type.php' );
+
             $addons[] = $addon;
 
             return $addons;
-        }
-
-        public static function plugins_loaded() {
-            require_once dirname( __FILE__ ) . '/component-type.php';
         }
 
     }
